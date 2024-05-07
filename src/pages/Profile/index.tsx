39e398a78e96ed1d8108481745bf4c500488d359
fortymiles <minchiuan@zju.edu.cn>
2024-05-07 19:43:57 -0400

    add exit

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -18,12 +18,25 @@ const Index: React.FC = () => {
 		}
 	}, []);
 
+	const handleExit = () => {
+		// Clearing the session storage or any other sign of user data
+		sessionStorage.clear();
+		// Optionally, you could reset local state if necessary
+		setHasSetProfile(false);
+		setProfile({});
+		// Redirecting the user to the root/homepage or a specific login page
+		navigate('/');
+	};
+
 	return (
 		<div id='Profile'>
 			{
 				hasSetProfile ? <MyProfile profile={profile} /> : <MyForm setHasSetProfile={setHasSetProfile} setProfile={setProfile} />
 			}
 			<Button className='btn-back' color='primary' fill='outline' onClick={() => navigate('/')}>Back</Button>
+			<div className="exit-button-container">
+				<Button className='btn-exit' color='danger' fill='outline' onClick={handleExit}>Exit</Button>
+			</div>
 		</div>
 	);
 };
